1e4369dbf97a81bdd25344c3c4903707506eae00
vanhuutoan27 <vhtoan27@gmail.com>
2024-11-06 00:04:30 +0700

    Update config.ts

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,5 +1,6 @@
 const projectName = "PureFood"
-const baseUrl = "https://localhost:7081/api/v1"
+// const baseUrl = "https://localhost:7081/api/v1"
+const baseUrl = "https://purefoods-a4hbh5g9bgfme5ca.southeastasia-01.azurewebsites.net/api/v1"
 const defaultAvatar =
   "https://firebasestorage.googleapis.com/v0/b/diamoondb-1412.appspot.com/o/Test%2Fbabibii.jpg?alt=media&token=bce6047f-8836-4d32-a90e-800a1433aa90"
 const shippingFee = 30000
